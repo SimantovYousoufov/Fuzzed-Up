dc4da67edc5c9efe4875c122301604901b44e044
Simon Yousoufov <simon.yous@gmail.com>
2015-02-12 08:36:00 -0500

    More descriptive error

--- controllers/fuzz.js
+++ controllers/fuzz.js
@@ -39,7 +39,7 @@ exports.decode = function(req, res) {
 
             res.send($.html());
         } else {
-            res.send('Error occurred');
+            res.send('Unable to retrieve HTML body');
         }
     });
 };
